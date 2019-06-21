33ec614eca320cef427b9b30a7f11f18a5f98af5
David Glasser <glasser@davidglasser.net>
2019-06-21 16:06:41 -0700

    apollo-engine-reporting: Factor out node creation from extension
    This is so that for federated service backends we can just create the node tree
    and not the full trace.
    
    This PR should be a no-op.

--- packages/apollo-engine-reporting/src/extension.ts
+++ packages/apollo-engine-reporting/src/extension.ts
@@ -2,8 +2,6 @@ import { Request, Headers, WithRequired } from 'apollo-server-env';
 
 import {
   GraphQLResolveInfo,
-  responsePathAsArray,
-  ResponsePath,
   DocumentNode,
   ExecutionArgs,
   GraphQLError,
@@ -18,6 +16,7 @@ import {
   VariableValueOptions,
   SendValuesBaseOptions,
 } from './agent';
+import { EngineReportingTreeBuilder } from './treeBuilder';
 import { GraphQLRequestContext } from 'apollo-server-core/dist/requestPipelineAPI';
 
 const clientNameHeaderKey = 'apollographql-client-name';
@@ -33,8 +32,7 @@ const clientVersionHeaderKey = 'apollographql-client-version';
 export class EngineReportingExtension<TContext = any>
   implements GraphQLExtension<TContext> {
   public trace = new Trace();
-  private nodes = new Map<string, Trace.Node>();
-  private startHrTime!: [number, number];
+  private treeBuilder: EngineReportingTreeBuilder;
   private explicitOperationName?: string | null;
   private queryString?: string;
   private documentAST?: DocumentNode;
@@ -50,11 +48,12 @@ export class EngineReportingExtension<TContext = any>
       ...options,
     };
     this.addTrace = addTrace;
-    const root = new Trace.Node();
-    this.trace.root = root;
-    this.nodes.set(responsePathAsString(undefined), root);
     this.generateClientInfo =
       options.generateClientInfo || defaultGenerateClientInfo;
+
+    this.treeBuilder = new EngineReportingTreeBuilder({
+      rewriteError: options.rewriteError,
+    });
   }
 
   public requestDidStart(o: {
@@ -70,7 +69,7 @@ export class EngineReportingExtension<TContext = any>
     >;
   }): EndHandler {
     this.trace.startTime = dateToTimestamp(new Date());
-    this.startHrTime = process.hrtime();
+    this.treeBuilder.startTiming();
 
     // Generally, we'll get queryString here and not parsedQuery; we only get
     // parsedQuery if you're using an OperationStore. In normal cases we'll get
@@ -131,9 +130,9 @@ export class EngineReportingExtension<TContext = any>
     }
 
     return () => {
-      this.trace.durationNs = durationHrTimeToNanos(
-        process.hrtime(this.startHrTime),
-      );
+      const { durationNs, rootNode } = this.treeBuilder.stopTiming();
+      this.trace.durationNs = durationNs;
+      this.trace.root = rootNode;
       this.trace.endTime = dateToTimestamp(new Date());
 
       this.trace.fullQueryCacheHit = !!o.requestContext.metrics
@@ -186,144 +185,18 @@ export class EngineReportingExtension<TContext = any>
     _context: TContext,
     info: GraphQLResolveInfo,
   ): ((error: Error | null, result: any) => void) | void {
-    const path = info.path;
-    const node = this.newNode(path);
-    node.type = info.returnType.toString();
-    node.parentType = info.parentType.toString();
-    node.startTime = durationHrTimeToNanos(process.hrtime(this.startHrTime));
-
-    return () => {
-      node.endTime = durationHrTimeToNanos(process.hrtime(this.startHrTime));
-      // We could save the error into the trace here, but it won't have all
-      // the information that graphql-js adds to it later, like 'locations'.
-    };
+    return this.treeBuilder.willResolveField(info);
+    // We could save the error into the trace during the end handler, but it
+    // won't have all the information that graphql-js adds to it later, like
+    // 'locations'.
   }
 
   public didEncounterErrors(errors: GraphQLError[]) {
-    errors.forEach(err => {
-      // In terms of reporting, errors can be re-written by the user by
-      // utilizing the `rewriteError` parameter.  This allows changing
-      // the message or stack to remove potentially sensitive information.
-      // Returning `null` will result in the error not being reported at all.
-      const errorForReporting = this.rewriteError(err);
-
-      if (errorForReporting === null) {
-        return;
-      }
-
-      this.addError(errorForReporting);
-    });
-  }
-
-  private rewriteError(err: GraphQLError): GraphQLError | null {
-    if (this.options.rewriteError) {
-      // Before passing the error to the user-provided `rewriteError` function,
-      // we'll make a shadow copy of the error so the user is free to change
-      // the object as they see fit.
-
-      // At this stage, this error is only for the purposes of reporting, but
-      // this is even more important since this is still a reference to the
-      // original error object and changing it would also change the error which
-      // is returned in the response to the client.
-
-      // For the clone, we'll create a new object which utilizes the exact same
-      // prototype of the error being reported.
-      const clonedError = Object.assign(
-        Object.create(Object.getPrototypeOf(err)),
-        err,
-      );
-
-      const rewrittenError = this.options.rewriteError(clonedError);
-
-      // Returning an explicit `null` means the user is requesting that, in
-      // terms of Engine reporting, the error be buried.
-      if (rewrittenError === null) {
-        return null;
-      }
-
-      // We don't want users to be inadvertently not reporting errors, so if
-      // they haven't returned an explicit `GraphQLError` (or `null`, handled
-      // above), then we'll report the error as usual.
-      if (!(rewrittenError instanceof GraphQLError)) {
-        return err;
-      }
-
-      // We only allow rewriteError to change the message and extensions of the
-      // error; we keep everything else the same. That way people don't have to
-      // do extra work to keep the error on the same trace node. We also keep
-      // extensions the same if it isn't explicitly changed (to, eg, {}). (Note
-      // that many of the fields of GraphQLError are not enumerable and won't
-      // show up in the trace (even in the json field) anyway.)
-      return new GraphQLError(
-        rewrittenError.message,
-        err.nodes,
-        err.source,
-        err.positions,
-        err.path,
-        err.originalError,
-        rewrittenError.extensions || err.extensions,
-      );
-    }
-    return err;
-  }
-
-  private addError(error: GraphQLError): void {
-    // By default, put errors on the root node.
-    let node = this.nodes.get('');
-    if (error.path) {
-      const specificNode = this.nodes.get(error.path.join('.'));
-      if (specificNode) {
-        node = specificNode;
-      }
-    }
-
-    node!.error!.push(
-      new Trace.Error({
-        message: error.message,
-        location: (error.locations || []).map(
-          ({ line, column }) => new Trace.Location({ line, column }),
-        ),
-        json: JSON.stringify(error),
-      }),
-    );
-  }
-
-  private newNode(path: ResponsePath): Trace.Node {
-    const node = new Trace.Node();
-    const id = path.key;
-    if (typeof id === 'number') {
-      node.index = id;
-    } else {
-      node.fieldName = id;
-    }
-    this.nodes.set(responsePathAsString(path), node);
-    const parentNode = this.ensureParentNode(path);
-    parentNode.child.push(node);
-    return node;
-  }
-
-  private ensureParentNode(path: ResponsePath): Trace.Node {
-    const parentPath = responsePathAsString(path.prev);
-    const parentNode = this.nodes.get(parentPath);
-    if (parentNode) {
-      return parentNode;
-    }
-    // Because we set up the root path in the constructor, we now know that
-    // path.prev isn't undefined.
-    return this.newNode(path.prev!);
+    this.treeBuilder.didEncounterErrors(errors);
   }
 }
 
 // Helpers for producing traces.
-
-// Convert from the linked-list ResponsePath format to a dot-joined
-// string. Includes the full path (field names and array indices).
-function responsePathAsString(p: ResponsePath | undefined) {
-  if (p === undefined) {
-    return '';
-  }
-  return responsePathAsArray(p).join('.');
-}
 
 // Converts a JS Date into a Timestamp.
 function dateToTimestamp(date: Date): google.protobuf.Timestamp {
@@ -333,24 +206,6 @@ function dateToTimestamp(date: Date): google.protobuf.Timestamp {
     seconds: (totalMillis - millis) / 1000,
     nanos: millis * 1e6,
   });
-}
-
-// Converts an hrtime array (as returned from process.hrtime) to nanoseconds.
-//
-// ONLY CALL THIS ON VALUES REPRESENTING DELTAS, NOT ON THE RAW RETURN VALUE
-// FROM process.hrtime() WITH NO ARGUMENTS.
-//
-// The entire point of the hrtime data structure is that the JavaScript Number
-// type can't represent all int64 values without loss of precision:
-// Number.MAX_SAFE_INTEGER nanoseconds is about 104 days. Calling this function
-// on a duration that represents a value less than 104 days is fine. Calling
-// this function on an absolute time (which is generally roughly time since
-// system boot) is not a good idea.
-//
-// XXX We should probably use google.protobuf.Duration on the wire instead of
-// ever trying to store durations in a single number.
-function durationHrTimeToNanos(hrtime: [number, number]) {
-  return hrtime[0] * 1e9 + hrtime[1];
 }
 
 function defaultGenerateClientInfo({ request }: GraphQLRequestContext) {
